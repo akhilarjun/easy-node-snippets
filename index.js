8844aa3b0d6588592c49c2824173d4ef1b489d64
Akhil Arjun <akhilparjun@gmail.com>
2020-07-01 14:44:34 +0530

    properly exported express web app

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let expressAppForWeb = require('./express-server-boilerplate-for-web');
 
-expressAppForWeb.app.get('/dummy-web', (req, res) => {
-    res.send('This call is working with new stuff');
-});
-
-expressAppForWeb.run(3030);
+module.exports = {
+    expressWebApp: expressAppForWeb
+}
